b7a5017f5e94bd7d9aea1a94a91aec22428f97ba
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-09-04 16:49:21 +0300

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./features/auth/components/Login.page";
+import LoginPage from "./features/auth/components/Login.page";
 import RegisterPage from "./features/auth/components/Register.page";
 import ProtectedRoute from "./features/auth/ProtectedRoute";
 import { PasswordResetFlow } from "./features/auth/components/ForgotPassword.page";
-import DashboardLayout from "./features/dashboard/components/DashboardLayout";
 import { Toaster } from "react-hot-toast";
 import CategoryManagement from "./features/categories/components/CategoryManagement";
 import CreateUser from "./features/users/components/CreateUser";
 import UpdateUser from "./features/users/components/UpdateUser";
-import Dashboard from "./features/dashboard/components/Dashboard.page";
 import EventManagement from "./features/events/components/EventManagement";
 import UsersList from "./features/users/components/UsersList";
 import UserProfile from "./features/users/components/UserProfile";
@@ -18,8 +16,10 @@ import UpdateEvent from "./features/events/components/UpdateEventPage";
 import BookTicket from "./features/tickets/components/BookTicket";
 import MyTickets from "./features/tickets/components/MyTickets";
 import AllTickets from "./features/tickets/components/AllTickets";
-import CreateCategoryPage from "./features/categories/components/CreateCategory";
 import UpdateCategoryPage from "./features/categories/components/UpdateCategory";
+import Dashboard from "./features/dashboard/components/Dashboard.page";
+import DashboardLayout from "./features/dashboard/components/DashboardLayout";
+import CreateCategoryPage from "./features/categories/components/CreateCategory";
 
 function App() {
   return (
@@ -28,7 +28,7 @@ function App() {
       <Toaster />
       <Routes>
         {/* Public Routes */}
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<PasswordResetFlow />} />
 
